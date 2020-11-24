26bba84407b9ef0b04de5530311abf875b0e88d5
graeme-young <graeme.patrick.young@gmail.com>
2020-11-23 20:26:13 -0500

    Finished styling index page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,4 @@
 import {
-  Link as ChakraLink,
   Text,
   Code,
   Icon,
@@ -11,24 +10,30 @@ import {
   Flex,
   Heading,
   Link,
+  IconButton,
 } from "@chakra-ui/core";
 import axios from "axios";
-import { useEffect, useState } from "react";
-import { useInfiniteQuery } from "react-query";
+import { AiOutlineHeart, AiOutlineShopping } from "react-icons/ai";
+import { BsPerson } from "react-icons/bs";
+import { HiOutlineShoppingCart } from "react-icons/hi";
 import { useQuery } from "react-query";
 import { Item } from "../../types";
 import { ItemPreviewCard } from "../components/ItemPreviewCard";
 import { Navbar } from "../components/Navbar";
 
 const Index = () => {
-  const { data, isLoading, error, isError } = useQuery(
+  const { data: itemData, isLoading, error, isError } = useQuery(
     "fetchItems",
     async () => {
       return await axios.get("http://localhost:8080/api/items");
     }
   );
 
-  // useEffect(() => {}, [data]);
+  const { data: meData } = useQuery("fetchMe", async () => {
+    return await axios.get("http://localhost:8080/api/users/me");
+  });
+
+  const itemBody = [];
 
   if (isLoading) {
     return <p>...Loading</p>;
@@ -43,28 +48,58 @@ const Index = () => {
     );
   }
 
-  if (data) {
-    if (data.data.length > 3) {
-      data.data.slice();
-    }
-  }
-
   return (
     <>
       <Navbar />
-      <Box ml={15}>
-        <Heading>Welcome to the Cushion!</Heading>
+      <Box ml={15} textAlign="center">
+        <Heading>Welcome to The Cushion!</Heading>
         <Text>The world's greatest fake furntiure store</Text>
         <Text>Browse fake couches, chairs, tables and more!</Text>
+        <br />
         <Text>
-          <Link>Log in</Link> to add items to your wishlist, shopping cart and
-          pretend to buy any of the items in our store!
+          View items by clicking the
+          <IconButton
+            icon={AiOutlineShopping}
+            fontSize={20}
+            aria-label="Shop"
+            variant="outline"
+          />{" "}
+          icon in the navigation bar.
         </Text>
+        {!meData?.data.success ? (
+          <Text>
+            You can also view the contents of your personal wishlist by clicking
+            the{" "}
+            <IconButton
+              icon={AiOutlineHeart}
+              fontSize={20}
+              aria-label="Wishlist"
+              variant="outline"
+            />{" "}
+            icon or your shopping cart using the{" "}
+            <IconButton
+              icon={HiOutlineShoppingCart}
+              fontSize={20}
+              aria-label="Cart"
+              variant="outline"
+            />{" "}
+            icon.
+          </Text>
+        ) : (
+          <Text>
+            Log in using the <Icon as={BsPerson} /> to add items to your
+            personal wishlist, shopping cart or to pretend to buy any of the
+            items in our store!
+          </Text>
+        )}
       </Box>
-      <Flex m={25} flexDir="row">
-        {data.data.map((item) => {
-          return <ItemPreviewCard item={item as Item} key={item.itemId} />;
-        })}
+      <Flex m={25} flexDir="row" justifyContent="center">
+        {itemData.data.reduce((itemArr, item, i) => {
+          if (i < 10 && i < itemData.data.length) {
+            itemArr.push(<ItemPreviewCard item={item} />);
+          }
+          return itemArr;
+        }, [])}
       </Flex>
     </>
   );
